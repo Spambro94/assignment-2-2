b082e1937bedf265f65f16dda5bd6523d263b885
Spambro94 <spambro94@gmail.com>
2025-03-26 12:37:58 +0000

    yes

---
+++ # This program calculates your age in th.py
@@ -0,0 +1,14 @@
+# This program calculates your age in the year 2050.
+# Input:  None
+# Output: Your current age followed by your age in 2050
+
+# Assign variables
+myCurrentAge = 30  
+currentYear = 2025 
+
+# Calculate age in 2050
+myNewAge = myCurrentAge + (2050 - currentYear)
+
+# Print results
+print("My Current Age is " + str(myCurrentAge))
+print("I will be " + str(myNewAge) + " in 2050.")
